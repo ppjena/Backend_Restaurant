528396322205ca0c83cf23123cd509e09b68a603
pragyan <ppjena@ncsu.edu>
2015-11-29 17:19:01 -0800

    swagger for Owner

--- RRS_App/src/io/egen/rrs/OwnerControl.java
+++ RRS_App/src/io/egen/rrs/OwnerControl.java
@@ -1,7 +1,5 @@
 package io.egen.rrs;
-
 import java.util.List;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
@@ -10,10 +8,8 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import io.egen.beans.OwnerBean;
 import io.egen.beans.ProfileBean;
 import io.egen.beans.ReservationBean;
@@ -25,20 +21,24 @@ import io.egen.dao.owner.ReservationListDAO;
 import io.egen.dao.owner.SeatingDAO;
 import io.egen.dao.owner.TableDAO;
 import io.egen.utils.DAOException;
-
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 @Path("owner")
+@Api(tags = { "owner" })
 public class OwnerControl {
 
-	@Path("loginGet/{username}/{password}")
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public String loginGet(@PathParam("username") String username, @PathParam("password") String password) {
-		return login(username, password);
-	}
-
+	/**
+	 * Login of Owner
+	 */
 	@Path("login")
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
+	@ApiOperation(value = "Login", notes = "Login of the Owner")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Success"),
+			@ApiResponse(code = 400, message = "Bad Request"),
+			@ApiResponse(code = 500, message = "Internal Service Error") })
 	public String login(@QueryParam("username") String username, @QueryParam("password") String password) {
 		try {
 			OwnerBean ownerBean = new LoginDAO().login(username, password);
@@ -48,10 +48,17 @@ public class OwnerControl {
 		}
 		return "Error";
 	}
-
+	
+	/**
+	 * View list of Reservations
+	 */
 	@Path("listReservations/{date}")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
+	@ApiOperation(value = "View Reservation", notes = "View Reservation List by the Owner for a particular date")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Success"),
+			@ApiResponse(code = 404, message = "Not Found"),
+			@ApiResponse(code = 500, message = "Internal Service Error") })
 	public String listReservations(@PathParam("date") String date) {
 		try {
 			List<ReservationBean> reservationList = new ReservationListDAO().generateReservationList(date);
@@ -61,10 +68,17 @@ public class OwnerControl {
 		}
 		return "Error";
 	}
-
+	
+	/**
+	 * View profile of the restaurant
+	 */
 	@Path("getProfile")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
+	@ApiOperation(value = "View profile", notes = "View profile of the restaurant by the Owner")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Success"),
+			@ApiResponse(code = 404, message = "Not Found"),
+			@ApiResponse(code = 500, message = "Internal Service Error") })
 	public String getProfile() {
 		try {
 			ProfileBean profile = new ProfileDAO().getProfileDetails();
@@ -74,20 +88,18 @@ public class OwnerControl {
 		}
 		return "Error";
 	}
-
-	@Path("editProfileGet/{name}/{contact}/{email}/{address}/{autoAssign}/{opening}/{closing}/{openDays}")
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public String editProfileGet(@PathParam("name") String name, @PathParam("contact") String contact,
-			@PathParam("email") String email, @PathParam("address") String address,
-			@PathParam("autoAssign") int autoAssign, @PathParam("opening") String opening,
-			@PathParam("closing") String closing, @PathParam("openDays") String openDays) {
-		return editProfile(name, contact, email, address, autoAssign, opening, closing, openDays);
-	}
-
+	
+	/**
+	 * Edit profile of the restaurant
+	 */
 	@Path("editProfile")
-	@POST
+	@PUT
 	@Produces(MediaType.APPLICATION_JSON)
+	@ApiOperation(value = "Edit profile", notes = "Edit profile of the restaurant by the Owner")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Success"),
+			@ApiResponse(code = 400, message = "Bad Request"),
+			@ApiResponse(code = 404, message = "Not Found"),
+			@ApiResponse(code = 500, message = "Internal Service Error") })
 	public String editProfile(@QueryParam("name") String name, @QueryParam("contact") String contact,
 			@QueryParam("email") String email, @QueryParam("address") String address,
 			@QueryParam("autoAssign") int autoAssign, @QueryParam("opening") String opening,
@@ -102,10 +114,17 @@ public class OwnerControl {
 		}
 		return "Error";
 	}
-
+	
+	/**
+	 * View contact lists of the customers
+	 */
 	@Path("contactList")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
+	@ApiOperation(value = "View contactList", notes = "View contactList of the customers by the Owner")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Success"),
+			@ApiResponse(code = 404, message = "Not Found"),
+			@ApiResponse(code = 500, message = "Internal Service Error") })
 	public String contactList() {
 		try {
 			List<String> contactList = new ContactListDAO().getContactList();
@@ -115,10 +134,17 @@ public class OwnerControl {
 		}
 		return "Error";
 	}
-
+	
+	/**
+	 * View reservation lists of the a particular customer
+	 */
 	@Path("reservationsForContact/{phonenumber}")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
+	@ApiOperation(value = "View reservation for Contact", notes = "View list of resservations of a particular customer from his contact number by the Owner")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Success"),
+			@ApiResponse(code = 404, message = "Not Found"),
+			@ApiResponse(code = 500, message = "Internal Service Error") })
 	public List<ReservationBean> contactList(@PathParam("phonenumber") String phonenumber) {
 		try {
 			List<ReservationBean> reservationList = new ReservationListDAO()
@@ -129,10 +155,18 @@ public class OwnerControl {
 		}
 		return null;
 	}
-
+	
+	
+	/**
+	 * View list of table details by the owner
+	 */
 	@Path("tableList/{date}")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
+	@ApiOperation(value = "View table details", notes = "View list od table details by the owner")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Success"),
+			@ApiResponse(code = 404, message = "Not Found"),
+			@ApiResponse(code = 500, message = "Internal Service Error") })
 	public List<TableBean> tableList(@PathParam("date") String date) {
 		try {
 			List<TableBean> tableList = new TableDAO().generateTableLists(date);
@@ -142,12 +176,20 @@ public class OwnerControl {
 		}
 		return null;
 	}
-
-	@Path("assignTable/{confirmationCode}/{tableName}")
+	
+	/**
+	 * Assign or Change Table by the Owner
+	 */
+	@Path("assignTable")
 	@PUT
 	@Produces(MediaType.APPLICATION_JSON)
-	public String assignTable(@PathParam("confirmationCode") String confirmationCode,
-			@PathParam("tableName") String tableName) {
+	@ApiOperation(value = "Assign/Change Table", notes = "Assign or change table by the Owner")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Success"),
+			@ApiResponse(code = 400, message = "Bad Request"),
+			@ApiResponse(code = 404, message = "Not Found"),
+			@ApiResponse(code = 500, message = "Internal Service Error") })
+	public String assignTable(@QueryParam("confirmationCode") String confirmationCode,
+			@QueryParam("tableName") String tableName) {
 		try {
 			new SeatingDAO().updateTable(confirmationCode, tableName);
 			return "SUCCESS";
